9bfda60cb992df5e58af3e9e8779f1247695c4a8
Ritul <ritulkevadiya2802@gmai.com>
2024-01-22 22:06:24 +0530

    initial commit

---
+++ src/main/java/com/example/railwattest/RailwatTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.railwattest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwatTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwatTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/railwattest/RailwatTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railwattest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwatTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
